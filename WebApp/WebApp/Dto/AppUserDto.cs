53b4412721bc8a89418dc62774e7a94419130a32
Artur Nowak <arturmareknowal@gmail.com>
2021-08-26 19:59:55 +0200

    added users documentation

--- WebApp/WebApp/Dto/AppUserDto.cs
+++ WebApp/WebApp/Dto/AppUserDto.cs
@@ -6,15 +6,27 @@ namespace WebApp.Dto
 {
     public sealed class AppUserDto
     {
+        /// <summary>
+        /// User's email address
+        /// </summary>
         [Required(AllowEmptyStrings = false)]
         public string Email { get; set; } = string.Empty;
 
+        /// <summary>
+        /// User's first name
+        /// </summary>
         [Required(AllowEmptyStrings = false)]
         public string FirstName { get; set; } = string.Empty;
 
+        /// <summary>
+        /// User's last name
+        /// </summary>
         [Required(AllowEmptyStrings = false)]
         public string LastName { get; set; } = string.Empty;
         
+        /// <summary>
+        /// User's password in plaintext
+        /// </summary>
         [Required(AllowEmptyStrings = false)]
         public string PasswordPlainText { get; set; } = string.Empty;
     }
