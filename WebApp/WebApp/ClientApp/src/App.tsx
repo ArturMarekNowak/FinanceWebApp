056d581a451c32a430ec30c2c4891e6651650219
Artur Nowak <arturmareknowal@gmail.com>
2021-11-01 00:00:49 +0100

    made hello world component from the scratch

--- WebApp/WebApp/ClientApp/src/App.tsx
+++ WebApp/WebApp/ClientApp/src/App.tsx
@@ -1,20 +1,17 @@
 import * as React from 'react';
-import { Route } from 'react-router';
-import Layout from './components/NavMenu/Layout';
-import Home from './components/Home/Home';
-import Counter from './components/Home/Counter';
-import FetchData from './components/Home/FetchData';
-import Login from './components/Home/Login';
-import Register from './components/Home/Register';
 
-import './custom.css'
+function App() {
+    return (
+        <div>
+            <h1>My Todos</h1>
+            <div className='card'>
+                <h2>Title</h2>
+                <div className='actions'>
+                    <button className='btn'>Delete</button>
+                </div>
+            </div>
+        </div>
+    );
+}
 
-export default () => (
-    <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/Home/counter' component={Counter} />
-        <Route path='/Home/fetch-data/:startDateIndex?' component={FetchData} />
-        <Route path='/Home/login' component={Login} />
-        <Route path='/Home/register' component={Register} />
-    </Layout>
-);
+export default App;
