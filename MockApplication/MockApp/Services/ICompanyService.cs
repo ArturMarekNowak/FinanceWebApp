ff1712a5edb87eb907fcc735c513b8bd96769479
Kruza <macik1@hotmail.com>
2021-11-07 18:16:20 +0100

    Initial mock app version. Adaded price generation - basic version

---
+++ MockApplication/MockApp/Services/ICompanyService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MockApp.Models;
+
+namespace MockApp.Services
+{
+    public interface ICompanyService
+    {
+        IEnumerable<CompanyShare> Get();
+    }
+}
